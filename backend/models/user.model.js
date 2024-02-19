6011ff3999cc738e22b405b0e8e93e66f9d54978
mr-Dev-Anshu <anshur9608837@gmail.com>
2024-02-19 12:55:10 +0530

    building usermodel

---
+++ backend/models/user.model.js
@@ -0,0 +1,13 @@
+import mongoose, { Schema } from "mongoose";
+  const userSchema = new Schema ({
+      email : {
+         type:String,
+         required:true ,
+         unique : true 
+      } ,
+      password: {
+          type:String ,
+          required:true ,
+          unique : true 
+      }
+  })
